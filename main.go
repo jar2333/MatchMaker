9b7cd13b1a1f0be32799a6df23368cae14961322
jar2333 <jar2333@columbia.edu>
2023-06-16 20:37:14 -0400

    refactored tournament functionality into a struct/method

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"os"
 	"strings"
-	"sync"
 
 	"github.com/gorilla/websocket"
 )
@@ -20,9 +19,6 @@ var upgrader = websocket.Upgrader{
 
 var KEYS = map[string]bool{"testkey": true}
 
-var reg registry = makeRegistry()
-var games chan game = make(chan game)
-
 var has_tournament_started bool = false
 
 // =============================
@@ -30,8 +26,6 @@ var has_tournament_started bool = false
 // =============================
 
 func main() {
-	defer reg.close()
-
 	// Get command-line arguments
 	arguments := os.Args
 	if len(arguments) == 1 {
@@ -42,8 +36,13 @@ func main() {
 	// Load player keys from file
 	loadKeys()
 
+	// Create tournament
+	tournament := makeTournament()
+
 	// Set http handler and start server
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handler(tournament, w, r)
+	})
 
 	port := ":" + arguments[1]
 	err := http.ListenAndServe(port, nil)
@@ -60,21 +59,10 @@ func main() {
 
 	// Play tournament until finishing
 	has_tournament_started = true
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	go func() {
-		defer wg.Done()
-		defer close(games)
-		go playGames() // Ends when games channel is closed
-		matchMake()    // Ends when tournament is over
-	}()
-
-	wg.Wait() // Wait until tournament is over
+	tournament.Start()
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handler(t *tournament, w http.ResponseWriter, r *http.Request) {
 	// If tournament has already started, accept no new connections
 	if has_tournament_started {
 		return
@@ -95,7 +83,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	reg.registerPlayer(key, conn)
+	t.Register(key, conn)
 }
 
 func getKey(conn *websocket.Conn) (string, bool) {
@@ -148,127 +136,4 @@ func SplitLines(s string) []string {
 		lines = append(lines, sc.Text())
 	}
 	return lines
-}
-
-// =============================
-// = Game-playing goroutines
-// =============================
-
-func playGames() {
-	for g := range games {
-		go playGame(g)
-	}
-}
-
-func playGame(g game) {
-	// Get player keys for this game
-	p1 := g.P1()
-	p2 := g.P2()
-
-	// Grab a reference to the websockets corresponding to player 1 and player 2
-	conn1 := reg.getConnection(p1)
-	conn2 := reg.getConnection(p2)
-
-	// Game loop until game is finished and winner is found:
-	var msg []byte
-	var played bool
-	for !g.IsFinished() {
-		// Parse player 1's move, perform it, send game state
-		played = false
-		for !played {
-			msg = readMessage(conn1)
-			played = g.Play(p1, parseMove(msg))
-		}
-		sendState(conn1, g)
-
-		if g.IsFinished() {
-			break
-		}
-
-		// Parse player 2's move, perform it, send game state
-		played = false
-		for !played {
-			msg = readMessage(conn2)
-			played = g.Play(p2, parseMove(msg))
-		}
-		sendState(conn2, g)
-	}
-
-}
-
-func readMessage(conn *websocket.Conn) []byte {
-	for {
-		// Read message from websocket connection
-		_, msg, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		return msg
-	}
-}
-
-func sendState(conn *websocket.Conn, g game) {
-	// Not yet implemented
-}
-
-// =============================
-// = Match-making goroutines
-// =============================
-
-func matchMake() {
-	// START: create tournament schedule
-	registered := reg.getRegistered()
-
-	schedule := getSchedule(registered)
-
-	// Play all games
-	for _, round := range schedule {
-		var wg sync.WaitGroup
-
-		results := make(chan string)
-
-		for _, pair := range round {
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				winner := evalGame(pair)
-				results <- winner
-			}()
-		}
-
-		wg.Wait()
-
-		for winner := range results {
-			reg.recordWin(winner)
-		}
-
-	}
-}
-
-func evalGame(p pair) string {
-	p1 := p.p1
-	p2 := p.p2
-
-	if (p1 == EMPTY_KEY || reg.isDisqualified(p1)) && (p2 == EMPTY_KEY || reg.isDisqualified(p2)) {
-		// No winner
-		return EMPTY_KEY
-	} else if p1 == EMPTY_KEY || reg.isDisqualified(p1) {
-		// Player 1 loses, Player 2 wins
-		return p2
-	} else if p2 == EMPTY_KEY || reg.isDisqualified(p2) {
-		// Player 2 loses, Player 1 wins
-		return p1
-	}
-
-	// Create new game
-	game := makeGame(p1, p2)
-
-	// Send game to games channel
-	games <- game
-
-	// Get winner of game from channel
-	winner := game.WaitForWinner()
-
-	return winner
 }
---
+++ tournament.go
@@ -0,0 +1,171 @@
+package tournament
+
+import (
+	"log"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type tournament struct {
+	reg   registry
+	games chan game
+}
+
+func makeTournament() *tournament {
+	return &tournament{
+		reg:   makeRegistry(),
+		games: make(chan game),
+	}
+}
+
+func (t *tournament) Register(key string, conn *websocket.Conn) {
+	t.reg.registerPlayer(key, conn)
+}
+
+func (t *tournament) Start() {
+	defer close(t.games)
+	defer t.reg.close()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		go t.playGames() // Ends when games channel is closed
+		t.matchMake()    // Ends when tournament is over
+	}()
+
+	wg.Wait() // Wait until tournament is over
+}
+
+// =============================
+// = Game-playing goroutines
+// =============================
+
+func (t *tournament) playGames() {
+	for g := range t.games {
+		go t.playGame(g)
+	}
+}
+
+func (t *tournament) playGame(g game) {
+	// Get registry
+	reg := &t.reg
+
+	// Get player keys for this game
+	p1 := g.P1()
+	p2 := g.P2()
+
+	// Grab a reference to the websockets corresponding to player 1 and player 2
+	conn1 := reg.getConnection(p1)
+	conn2 := reg.getConnection(p2)
+
+	// Game loop until game is finished and winner is found:
+	var msg []byte
+	var played bool
+	for !g.IsFinished() {
+		// Parse player 1's move, perform it, send game state
+		played = false
+		for !played {
+			msg = readMessage(conn1)
+			played = g.Play(p1, parseMove(msg))
+		}
+		sendState(conn1, g)
+
+		if g.IsFinished() {
+			break
+		}
+
+		// Parse player 2's move, perform it, send game state
+		played = false
+		for !played {
+			msg = readMessage(conn2)
+			played = g.Play(p2, parseMove(msg))
+		}
+		sendState(conn2, g)
+	}
+
+}
+
+func readMessage(conn *websocket.Conn) []byte {
+	for {
+		// Read message from websocket connection
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		return msg
+	}
+}
+
+func sendState(conn *websocket.Conn, g game) {
+	// Not yet implemented
+}
+
+// =============================
+// = Match-making goroutines
+// =============================
+
+func (t *tournament) matchMake() {
+	// Get registry
+	reg := &t.reg
+
+	// START: create tournament schedule
+	registered := reg.getRegistered()
+
+	schedule := getSchedule(registered)
+
+	// Play all games
+	for _, round := range schedule {
+		var wg sync.WaitGroup
+
+		results := make(chan string)
+
+		for _, pair := range round {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				winner := t.evalGame(pair)
+				results <- winner
+			}()
+		}
+
+		wg.Wait()
+
+		for winner := range results {
+			reg.recordWin(winner)
+		}
+
+	}
+}
+
+func (t *tournament) evalGame(p pair) string {
+	p1 := p.p1
+	p2 := p.p2
+
+	reg := &t.reg
+
+	if (p1 == EMPTY_KEY || reg.isDisqualified(p1)) && (p2 == EMPTY_KEY || reg.isDisqualified(p2)) {
+		// No winner
+		return EMPTY_KEY
+	} else if p1 == EMPTY_KEY || reg.isDisqualified(p1) {
+		// Player 1 loses, Player 2 wins
+		return p2
+	} else if p2 == EMPTY_KEY || reg.isDisqualified(p2) {
+		// Player 2 loses, Player 1 wins
+		return p1
+	}
+
+	// Create new game
+	game := makeGame(p1, p2)
+
+	// Send game to games channel
+	t.games <- game
+
+	// Get winner of game from channel
+	winner := game.WaitForWinner()
+
+	return winner
+}
