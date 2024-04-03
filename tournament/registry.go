8a8449c92c61ec54f1135c171300e6280aa8a833
neonet1 <38730172+neonet1@users.noreply.github.com>
2024-04-03 18:58:00 -0400

    Changed function names to be public

--- tournament/registry.go
+++ tournament/registry.go
@@ -6,42 +6,42 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-type registry struct {
+type Registry struct {
 	connections  map[string]*websocket.Conn
 	win_count    map[string]int
 	disqualified map[string]bool
 	lock         sync.RWMutex
 }
 
-func makeRegistry() registry {
-	return registry{win_count: make(map[string]int), disqualified: make(map[string]bool), lock: sync.RWMutex{}}
+func MakeRegistry() Registry {
+	return Registry{win_count: make(map[string]int), disqualified: make(map[string]bool), lock: sync.RWMutex{}}
 }
 
-func (r *registry) close() {
+func (r *Registry) Close() {
 	for _, conn := range r.connections {
 		conn.Close()
 	}
 }
 
-func (r *registry) getConnection(key string) *websocket.Conn {
+func (r *Registry) GetConnection(key string) *websocket.Conn {
 	return r.connections[key]
 }
 
-func (r *registry) registerPlayer(key string, conn *websocket.Conn) {
+func (r *Registry) RegisterPlayer(key string, conn *websocket.Conn) {
 	r.lock.Lock()
 	defer r.lock.Unlock()
 	r.win_count[key] = 0
 	r.connections[key] = conn
 }
 
-func (r *registry) deregisterPlayer(key string) {
+func (r *Registry) DeregisterPlayer(key string) {
 	r.lock.Lock()
 	defer r.lock.Unlock()
 	delete(r.win_count, key)
 	delete(r.connections, key)
 }
 
-func (r *registry) isRegistered(key string) bool {
+func (r *Registry) IsRegistered(key string) bool {
 	r.lock.Lock()
 	defer r.lock.Unlock()
 
@@ -50,13 +50,13 @@ func (r *registry) isRegistered(key string) bool {
 	return ok
 }
 
-func (r *registry) disqualifyPlayer(key string) {
+func (r *Registry) DisqualifyPlayer(key string) {
 	r.lock.Lock()
 	defer r.lock.Unlock()
 	r.disqualified[key] = true
 }
 
-func (r *registry) isDisqualified(key string) bool {
+func (r *Registry) IsDisqualified(key string) bool {
 	r.lock.Lock()
 	defer r.lock.Unlock()
 	d, ok := r.disqualified[key]
@@ -68,7 +68,7 @@ func (r *registry) isDisqualified(key string) bool {
 	return d
 }
 
-func (r *registry) getRegistered() []string {
+func (r *Registry) GetRegistered() []string {
 	keys := make([]string, len(r.win_count))
 
 	i := 0
@@ -80,7 +80,7 @@ func (r *registry) getRegistered() []string {
 	return keys
 }
 
-func (r *registry) recordWin(key string) {
+func (r *Registry) RecordWin(key string) {
 	r.lock.Lock()
 	defer r.lock.Unlock()
 	r.win_count[key] += 1
